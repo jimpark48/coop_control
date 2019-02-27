dbd24689dbe0aa5809bc7260575ea9216cecc3c0
jimpark48 <46771708+jimpark48@users.noreply.github.com>
2019-02-27 18:17:42 +0900

    Add files via upload

--- include/coop_control/coop_control.h
+++ include/coop_control/coop_control.h
@@ -1,54 +1,60 @@
-#ifndef coop_control_coop_control_H
-#define coop_control_coop_control_H
+#ifndef COOP_CONTROL_COOP_CONTROL_H
+#define COOP_CONTROL_COOP_CONTROL_H
 
 #include <rqt_gui_cpp/plugin.h>
 #include <coop_control/ui_coop_control.h>
 #include <QWidget>
+
 //using QPushButton
 #include <QPushButton>
 #include <QApplication>
 
-#include <ros/ros.h>
-#include <std_msgs/Int32.h>
-//using QListView 
+//using QListView
 #include <QString>
 #include <QStringList>
 #include <QStringListModel>
 
+//using QTextEdit
+#include <QTextEdit>
+
+#include "ros/ros.h"
+
 namespace coop_control
 {
-
 class coopPlugin
-  : public rqt_gui_cpp::Plugin
+    : public rqt_gui_cpp::Plugin
 {
-  Q_OBJECT
+Q_OBJECT
 public:
   coopPlugin();
 
   virtual void initPlugin(qt_gui_cpp::PluginContext& context);
   virtual void shutdownPlugin();
   virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings,
-      qt_gui_cpp::Settings& instance_settings) const;
-  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings,
-      const qt_gui_cpp::Settings& instance_settings);
+        qt_gui_cpp::Settings& instance_settings) const;    
+  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, 
+        const qt_gui_cpp::Settings& instance_settings);
 
   virtual void connectionfunc();
-
-  // Comment in to signal that the plugin has a way to configure it
-  // bool hasConfiguration() const;
-  // void triggerConfiguration();
+  //comment in to signal that the plugin has a way to configure it
+  //bool hasConfiguration() const;
+  //void triggerConfiguration();
 
 private:
-  Ui::MyPluginWidget ui_;
+  Ui::coopwidget ui_;
   QWidget* widget_;
   QStringListModel* model;
+
+  ros::NodeHandle nh;
+  ros::Publisher gain_pub;
 
   Q_SIGNALS:
     void clicked();
 
-private slots:
-  void on_enterButton_clicked(); //반드시 slots 안에 정의할 것
-
+  private slots:
+    void on_enterButton_clicked();
+    void on_gainplus_clicked();
+    void on_gainminus_clicked();
 };
-}  // namespace coop_control
-#endif  // RQT_AIDIN_coop_control_H
+} //namespace coop_control
+#endif //COOP_CONTROL_COOP_CONTROL_H
